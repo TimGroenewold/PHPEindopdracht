00a4aaf96ecb5f043399916f4655d7f482ab4464
TimGroenewold <43601514+TimGroenewold@users.noreply.github.com>
2018-10-30 18:13:22 +0100

    New Stuff

---
+++ Myinterests.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <title>RssFeed</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    <body>
+        <div id="header"> 
+            <div class="navblock">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+                        <li><a href="Weather.php">Weather</a></li>
+                        <li><a href="Rssfeed.php">Rss Feed</a></li>
+                        <li><a href="Myinterests.php">My Interests</a></li>
+                        <li><a href="Infoform.php">Info Form</a></li>
+		</ul>
+            </div>
+        </div>  
+            <div id="content">
+                <?php
+                
+                ?>
+            </div>
+    </body>
+</html>         
